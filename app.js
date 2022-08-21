d3da1992bcac82ca3e816e6b893d16d93724fa5e
Jose <elmomazotriste@gmail.com>
2022-08-21 00:32:39 -0500

    pagina terminada

---
+++ app.js
@@ -0,0 +1,7 @@
+const frutas = ['Manzana', 'Plátano', 'Naranja', 'Fresa', 'Zarzamora'];
+
+// Recorremos el arreglo
+frutas.forEach((elemento, indice, array) => {
+  // Imprime el elemento actual y su índice
+  console.log(elemento, indice);
+}); 
