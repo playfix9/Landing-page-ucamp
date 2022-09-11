5b70976659f3aedaae45d39be45c9b8822373786
Jose <elmomazotriste@gmail.com>
2022-09-11 14:03:34 -0500

    actualizacion final termino de la pagina web

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ frutas.forEach((elemento, indice, array) => {
   console.log(elemento, indice);
 }); 
 
-
-
 frutas.forEach((ola) =>{console.log(ola)});
+
+
+
+//ordenamos alfabeticamente
+frutas.sort((fruta1, fruta2) => {
+  // fruta1 va primero que fruta2
+  if (fruta1 < fruta2) {
+    return -1;
+  }
+  // fruta2 va primero que fruta1
+  if (fruta1 > fruta2) {
+    return 1;
+  }
+
+  // Los elementos ya están ordenados
+  return 0;
+});
+console.log('Frutas Ordenadas: ', frutas);
+
+
+
+
+     // Filtramos el arreglo
+     const berries = frutas.filter((elemento) => {
+      if (elemento === 'Fresa' || elemento === 'Zarzamora') {
+        return true;
+      }
+
+      return false;
+    });
+    console.log('Berries: ', berries);
